67b86c6d9fbe3553a5d6e969afdde027bd8ba7a4
Ranjit Das <ranjitda@creative.local>
2023-11-29 17:20:09 +0530

    Updated from laptop

--- app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -1,10 +1,8 @@
 package com.example.kotlinbasics
 
 fun main(){
-
-    var pie = 3.14f
-    println(pie)
-    pie = 3.1415926535f
-    println(pie)
-
+    var count = 0
+    while (count < 3){
+        println(count++)
+    }
 }
