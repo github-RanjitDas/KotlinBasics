b507f1f3a2b886f30db37ae7ce61465c8bdea129
Ranjit Das <ranjitda@creative.local>
2023-12-11 15:33:21 +0530

    Commited from laptop

--- app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -1,8 +1,23 @@
 package com.example.kotlinbasics
 
+
 fun main(){
-    var count = 0
-    while (count < 3){
-        println(count++)
-    }
+    makeCoffee(1, "Sukuta") //Calling the function
+    makeCoffee(2, "Ranjit") //Calling the function
+    makeCoffee(29, "Haria") //Calling the function
+
 }
+
+// Define function
+fun makeCoffee(sugarCount : Int, name : String) =
+    when(sugarCount){
+        0 -> {
+            println("Coffee with no sugar added for $name")
+        }
+        1 -> {
+            println("Coffee with $sugarCount spoon of sugar for $name")
+        }
+        else -> {
+            println("Coffee with $sugarCount spoons of sugar for $name")
+        }
+    }
